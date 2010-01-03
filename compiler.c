40ca00fcf67a6578f4e6a009b429e5e1c4275c23
Eric Herman <eric@freesa.org>
2010-01-03 03:06:40 -0500

    remove paren clutter

--- compiler.c
+++ compiler.c
@@ -7,25 +7,25 @@ int is_multiply_op(const char c);
 void expression(context_t * ctx) {
 	char c;
 
-	if ( is_add_op((ctx)->lex_look_ahead( (ctx)->data )) ) {
+	if ( is_add_op(ctx->lex_look_ahead(ctx->data)) ) {
 		/* okay, we have a "-(foo)" situation */
 		/* let's just slip-stream a zero. */
-		(ctx)->output_term( (ctx)->data, 0 );
+		ctx->output_term(ctx->data, 0);
 	} else {
 		term(ctx);
 	}
-	while (is_add_op(c = (ctx)->lex_look_ahead( (ctx)->data ))) {
+	while (is_add_op(c = ctx->lex_look_ahead(ctx->data))) {
 
-		(ctx)->lex_advance( (ctx)->data, 1 );
+		ctx->lex_advance( ctx->data, 1 );
 
 		term(ctx);
 
 		switch (c) {
 		case '+':
-			(ctx)->output_add( (ctx)->data );
+			ctx->output_add(ctx->data);
 			break;
 		case '-':
-			(ctx)->output_subtract( (ctx)->data );
+			ctx->output_subtract(ctx->data);
 		}
 	}
 }
@@ -34,33 +34,33 @@ void factor(context_t * ctx) {
 	char c;
 	int number;
 
-	c = (ctx)->lex_look_ahead( (ctx)->data );
+	c = ctx->lex_look_ahead(ctx->data);
 	if (c == '(') {
-		(ctx)->lex_advance( (ctx)->data, 1 );
+		ctx->lex_advance( ctx->data, 1 );
 		expression(ctx);
 		/* eat close paren */
-		(ctx)->lex_advance( (ctx)->data, 1 );
+		ctx->lex_advance( ctx->data, 1 );
 	} else {
-		number = (ctx)->lex_get_number( (ctx)->data );
-		(ctx)->output_term( (ctx)->data, number );
+		number = ctx->lex_get_number(ctx->data);
+		ctx->output_term( ctx->data, number );
 	}
 }
 
 void term(context_t * ctx) {
 	char c;
 	factor(ctx);
-	while( is_multiply_op( c = (ctx)->lex_look_ahead( (ctx)->data ) ) ) {
+	while( is_multiply_op( c = ctx->lex_look_ahead(ctx->data) ) ) {
 
-		(ctx)->lex_advance( (ctx)->data, 1 );
+		ctx->lex_advance( ctx->data, 1 );
 
 		factor(ctx);
 
 		switch (c) {
 		case '*':
-			(ctx)->output_multiply( (ctx)->data );
+			ctx->output_multiply(ctx->data);
 			break;
 		case '/':
-			(ctx)->output_divide( (ctx)->data );
+			ctx->output_divide(ctx->data);
 		}
 	}
 }
@@ -74,16 +74,16 @@ int is_multiply_op(const char c) {
 }
 
 void compile(context_t * ctx) {
-	(ctx)->read_line( (ctx)->data );
+	ctx->read_line(ctx->data);
 	compile_inner(ctx);
-	(ctx)->write_file( (ctx)->data );
+	ctx->write_file(ctx->data);
 }
 
 void compile_inner(context_t * ctx) {
-	(ctx)->output_header( (ctx)->data );
+	ctx->output_header(ctx->data);
 
 	expression(ctx);
 
-	(ctx)->output_statements_complete( (ctx)->data );
-	(ctx)->output_os_return( (ctx)->data );
+	ctx->output_statements_complete(ctx->data);
+	ctx->output_os_return(ctx->data);
 }
