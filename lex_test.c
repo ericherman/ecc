cefcebc772a4f0fd37bc84b10fe3d62e5cc78fa5
Eric Herman <eric@freesa.org>
2011-02-20 20:55:15 +0100

    split long test_lex_look_ahead into two tests

--- lex_test.c
+++ lex_test.c
@@ -14,38 +14,47 @@ void check_token(const char *input, unsigned int input_size,
 	check_strs(result, expected_result, "token");
 }
 
-void test_lex_look_ahead()
+void test_lex_look_ahead_1()
 {
 	const char *input = "1+ 2";
-	const char *input2 = "231+ 1421 - 7";
-	unsigned int pos, expected_pos;
+	unsigned int len, pos, expected_pos;
 	char *expected_result;
+
+	len = strlen(input);
 
 	pos = 0;
 	expected_pos = 0;
 	expected_result = "1";
-	check_token(input, strlen(input), &pos, expected_result, expected_pos);
+	check_token(input, len, &pos, expected_result, expected_pos);
 
 	pos = 1;
 	expected_pos = 1;
 	expected_result = "+";
-	check_token(input, strlen(input), &pos, expected_result, expected_pos);
+	check_token(input, len, &pos, expected_result, expected_pos);
 
 	pos = 2;
 	expected_pos = 3;
 	expected_result = "2";
-	check_token(input, strlen(input), &pos, expected_result, expected_pos);
+	check_token(input, len, &pos, expected_result, expected_pos);
+}
+
+void test_lex_look_ahead_2()
+{
+	const char *input = "231+ 1421 - 7";
+	unsigned int len, pos;
+
+	len = strlen(input);
 
 	pos = 0;
-	check_token(input2, strlen(input2), &pos, "231", 0);
+	check_token(input, len, &pos, "231", 0);
 	pos += 3;
-	check_token(input2, strlen(input2), &pos, "+", pos);
+	check_token(input, len, &pos, "+", pos);
 	pos += 1;
-	check_token(input2, strlen(input2), &pos, "1421", pos + 1);
+	check_token(input, len, &pos, "1421", pos + 1);
 	pos += 4;
-	check_token(input2, strlen(input2), &pos, "-", pos + 1);
+	check_token(input, len, &pos, "-", pos + 1);
 	pos += 1;
-	check_token(input2, strlen(input2), &pos, "7", pos + 1);
+	check_token(input, len, &pos, "7", pos + 1);
 }
 
 void test_lex_get_number()
@@ -83,7 +92,8 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	test_lex_look_ahead();
+	test_lex_look_ahead_1();
+	test_lex_look_ahead_2();
 	test_lex_get_number();
 	return 0;
 }
